c0bd076f97abeb899755ab98722e7b9f3c646395
root <root@ip-172-31-18-50.us-west-2.compute.internal>
2014-06-25 13:45:12 +0000

    Added CommentController and edited routes' ;

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,32 @@
 <?php
+// app/routes.php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+// ===================================
+// HOME PAGE =========================
+// ===================================
+Route::get('/', function() {
+	//we don't need to use Laravel Blade
+	// we will return a PHP file that will hold all of our Angular content
+	// see the "Where to Place Angular Files" to see ideas on how to structure
+	return View::make('index'); // will return app/views/index.php
+});
 
-Route::get('/', function()
-{
-	return View::make('hello');
+// ==================================
+// API ROUTES =======================
+// ==================================
+Route::group(array('prefix' => 'api'), function() {
+	// since we will be using this for CRUD, we won't need create and edit
+	// Angular will handle both those forms
+	// this ensures that a user can't access api/create or api/edit when nothing is there
+	Route::resource('comments', 'CommentController',
+	  array('only' => array('index','store', 'destroy')));
+});
+
+// ==================================
+// CATCH ALL ROUTE ==================
+// ==================================
+// all routes that are not home or api will be redirected to the frontend
+// this allows angular to route them
+App::missing(function($exception) {
+	return View::make('index');
 });
